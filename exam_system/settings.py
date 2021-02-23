261d080ae786403d3cec3b26828bbc8744b8f5a9
albatros-21 <57601999+albatros-21@users.noreply.github.com>
2021-02-24 01:34:39 +0300

    Update settings.py

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '75+b&b2!k&0=lge2-qew0coiyu5!vx4w+o=&j_y2##gch5g1(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
