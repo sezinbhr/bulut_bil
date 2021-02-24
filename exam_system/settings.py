c64a5bcf48fcd6c018e2a3ce10d5551ab5caa295
albatros-21 <05170000782@ogrenci.ege.edu.tr>
2021-02-24 18:01:55 +0300

    Update settings.py

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -76,15 +76,19 @@ WSGI_APPLICATION = 'exam_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-import dj_database_url
-
+import psycopg2
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baharsezinp1',
+        'USER':'sznbhr',
+        'PASSWORD': 'Alleranfang*12',
+        'HOST': 'baharsezinp1.postgres.database.azure.com',
+        'PORT': '5432',
          
     }
 }
+
 
 
 
