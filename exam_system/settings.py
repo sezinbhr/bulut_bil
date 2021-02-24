c0d23fdcf72411c525a2c28a4aa4d3bf9b165738
albatros-21 <57601999+albatros-21@users.noreply.github.com>
2021-02-24 04:53:29 +0300

    Update settings.py

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'exam_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'usrbhrszn',
         'PASSWORD': 'Alleranfang*23',
