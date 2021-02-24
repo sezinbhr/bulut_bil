5be31b7ed7f4e7e4730c58b152576f539fedd831
albatros-21 <57601999+albatros-21@users.noreply.github.com>
2021-02-24 09:05:04 +0300

    Update settings.py

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'exam_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import dj_database_url, psycopg2
+
 DATABASES = {
     'default': {
-             
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',                      
+        'USER':'usrbhrszn',
+        'PASSWORD': 'Alleranfang*23',
+        'HOST': 'baharsezinpostgres21.postgres.database.azure.com',
+        'PORT': '5432',
     }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
